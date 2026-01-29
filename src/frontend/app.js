d5018137c12653aa6977c88378b5e6f289c7958b
SzutaB01 <bartosz.szuta@studentfr.ch>
2026-01-29 09:17:25 +0100

    push

--- src/frontend/app.js
+++ src/frontend/app.js
@@ -1,5 +1,5 @@
 //const apiEndpoint = "https://fa-todo-backend-baqmes.azurewebsites.net/api/tasks";
-const apiEndpoint = "http://localhost:8080/api/tasks";
+const apiEndpoint = window.API_ENDPOINT;
 
 $(document).ready(function () {
   // Charger les tâches au démarrage
